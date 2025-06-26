d799c1c393db3f5c42b07c94cd4d601cb6f42a8a
ivonsteb <isabel.vonstebut@ugent.be>
2025-06-26 22:50:11 +0200

    add secondary whitening function for m sources (m<n)

--- src/ICA_BlindSourceSeparation.jl
+++ src/ICA_BlindSourceSeparation.jl
@@ -47,6 +47,66 @@ function whiten_dataset(dataset::sensorData)::sensorData
 end
 
 """
+    whiten_dataset(X::sensorData, m::Int64) -> sensorData, W::Matrix{Float64}, iW::Matrix{Float64}
+
+    Applies PCA whitening to TxN data matrix to decorrelate m sources
+    T: number of samples
+    N: number of sensors
+    m: number of sources
+    Returns the whitened dataset (Txm data matrix), whitening matrix W (mxn), pseudo-inverse whitening matrix iW (nxm)
+"""
+function whiten_dataset(dataset::sensorData, m::Int64)::sensorData
+    """
+    [U,D] 		= eig((X*X')/T)	; 
+    [puiss,k]	= sort(diag(D))	;
+    rangeW		= n-m+1:n			; % indices to the m  most significant directions
+    scales		= sqrt(puiss(rangeW))		; % scales
+    W  		= diag(1./scales)  * U(1:n,k(rangeW))'	;	% whitener
+    iW  		= U(1:n,k(rangeW)) * diag(scales) 	;	% its pseudo-inverse
+    X		= W*X;
+    """
+    
+    n_rows, n_cols = size(dataset.data)
+    if (length(dataset.time) != n_rows)
+        throw("Mismatch between time length and signal lengths")
+    end
+    if (n_cols == 0)
+        throw("Matrix must have at least two column.")
+    end
+    
+    # Copy to avoid modifying the original matrix
+    X = deepcopy(dataset.data)
+    T = size(X,1)
+    n = size(X,2)
+
+    if (m>n)
+        throw("More sources than sensors")
+    end
+
+    # center matrix
+    μ = mean(X, dims=1)
+    X_centered = X .- μ
+
+    # calculate sample covariance matrix (NxN)
+    Σ = cov(X, dims=1, corrected=false)
+
+    # eigendecomposition of Σ
+    eigenvals, U = eigen(Σ)
+
+    # select m largest eigenvalues
+    rangeW = n-m+1:n
+    scales = sqrt(eigenvals[rangeW])
+        
+    # calculate whitening matrix
+    W = Diagonal(1 ./scales) * U[:, rangeW]'
+    iW = U[:, rangeW] * Diagonal(scales)
+
+    data_white = X_centered * W'
+
+    return sensorData(dataset.time, data_white), W, iW
+end
+
+"""
     read_dataset(filename::String) -> sensorData
 
     Reads a file containing numbers separated by spaces or tabs.
--- src/JADE.jl
+++ src/JADE.jl
@@ -1,4 +1,22 @@
+#=------------------------------------------------------------
+JADE algorithm.
+Implements blind source separation by Joint Approximation Diagonalization of Eigen-matrices.
+Author: Isabel von Stebut
+Adapted from Jean-François Cardoso's MATLAB version
+=------------------------------------------------------------=#
+"""
+    JADE Algorithm for ICA source separation
+"""
+function ica_jade(dataset::sensorData, m::Int64)::sensorData
 
-function ica_jade(dataset::sensorData)::sensorData
+    # whitening & projection onto signal subspace
+    d = whiten_dataset(dataset, m)
+
+    # estimation of cumulant matrices
+    #---------------
+
+
+    # joint diagonalization of cumulant matrices
+    #---------------
 
 end
