b895aa2db1e2a8fad459c0c93b0dd2e6d3a06150
ivonsteb <isabel.vonstebut@ugent.be>
2025-06-28 01:34:28 +0200

    add joint diagonalization of cumulant matrices

--- src/JADE.jl
+++ src/JADE.jl
@@ -16,8 +16,10 @@ function ica_jade(dataset::sensorData, m::Int64)::sensorData
     CM = estimate_cumulants(d_white, m)
 
     # joint diagonalization of cumulant matrices
+    T = size(d_white.data, 1)
+    V = joint_diag(T, m, CM)
 
-    return sensorData(dataset.time, CM)
+    return sensorData(dataset.time, V)
 
 end
 
@@ -56,4 +58,55 @@ function estimate_cumulants(dataset::sensorData, m::Int64)
     
     return CM
 
+end
+
+
+"""
+Joint diagonalization of the cumulant matrices
+"""
+function joint_diag(T::Int64, m::Int64, CM_in::Matrix{Float64})
+    CM = copy(CM_in)
+    V = Matrix{Float64}(I, m, m)
+    thresh = 1/(100 * sqrt(T))
+    num_cumm = size(CM, 2) ÷ m
+
+    repeat = true
+    sweep = 0
+    updates = 0
+    while repeat
+        repeat = false
+        sweep += 1
+
+        for p = 1:m-1
+            for q = p+1:m
+                Ip = p:m:(m*num_cumm)
+                Iq = q:m:(m*num_cumm)
+
+                #compute givens angle
+                row1 = (@view(CM[p,Ip]) - @view(CM[q,Iq]))'
+                row2 = (@view(CM[p,Iq]) + @view(CM[q,Ip]))'
+                g = [row1 ; row2]
+                gg = g*g'
+                ton = gg[1,1] - gg[2,2]
+                toff = gg[1,2] + gg[2,1]
+                theta = 0.5 * atan(toff, ton+sqrt(ton^2 + toff^2))
+
+                # givens update
+                if abs(theta) > thresh
+                    repeat = true
+                    updates += 1
+                    c = cos(theta)
+                    s = sin(theta)
+                    G = [c -s ; s c]
+
+                    pair = [p , q]
+                    V[:,pair] = V[:,pair]*G
+                    CM[pair,:] = G' * CM[pair,:]
+                    all_indices = vcat(Ip, Iq)
+                    CM[:,all_indices] = [c*CM[:,Ip]+s*CM[:,Iq] -s*CM[:,Ip]+c*CM[:,Iq]]
+                end
+            end
+        end
+    end
+    return V
 end
