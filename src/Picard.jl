4fb4807357ee084bcf63383663937185f788b483
Misho <michail.todorov@gmail.com>
2025-07-01 15:45:53 +0200

    correction

--- src/Picard.jl
+++ src/Picard.jl
@@ -2,101 +2,72 @@ using LinearAlgebra
 
 function ica_picard(dataset::sensorData)
     
-    tol=1e-7
-    lambda_min=0.01
-    ls_tries=10
-    verbose=false
-    m=7
-    max_iter=1000
+    tol = 1e-7
+    lambda_min = 0.01
+    ls_tries = 10
+    verbose = false
+    m = 7
+    max_iter = 1000
 
-    X = deepcopy(dataset.data) # taking the data part of the parameter dataset
-    T, N = size(X) # saving the size of the dataset  - T rows for T points in time and N columns for N signals
-    W = Matrix{Float64}(I, N, N) # unmixing matrix, initialy identity matrix
-    Y=X # copying the data
+    X = deepcopy(dataset.data)
+    T, N = size(X)
+    W = Matrix{Float64}(I, N, N)
+    Y = X
 
-    # arrays for L-BFGS
     s_list = Vector{Matrix{Float64}}()
     y_list = Vector{Matrix{Float64}}()
     r_list = Vector{Float64}()
 
-    # signs array
     signs = ones(N)
-
-    # computing the loss
-    current_loss = compute_loss(Y, W, log_lik_tanh, signs; ortho=false, extended=true)
-
-    # flags
-    requested_tolerance = false # whether stopping tolerance has been reached
-    sign_change = false # whether signs flipped in current iteration
-
-    gradient_norm = 1.0 # initialising gradient norm
-
-    covariance = X' * X / T
-    C = copy(covariance)
+    current_loss = loss(Y, signs)
+    requested_tolerance = false
+    sign_change = false
 
     old_signs = copy(signs)
     G_old = zeros(N, N)
     direction = zeros(N, N)
 
     for n in 1:max_iter
-        
-        # Score function and its derivative
-        psiY = tanh.(Y)
-        psidY = 1 .- psiY .^ 2
-        
-        G = Y' * psiY / T # Relative gradient
+        psiY       = tanh.(Y)
+        psidY_mean = mean(1 .- psiY.^2, dims=1)[:]
+        g          = psiY' * Y / T
 
-        Y_square = Y .^ 2 # Squared signals
-
-        K = mean(psidY, dims=1)[:] .* diag(C)
-        K .-= diag(G)
+        K     = psidY_mean .* diag(g)
         signs = sign.(K)
-
         if n > 1
-            sign_change = any(signs .!= old_signs) # Sign flip
+            sign_change = any(signs .!= old_signs)
         end
-
         old_signs = signs
 
-        G .= G .* signs # Apply signs to gradient
-        psidY .= psidY .* signs
+        g .*= reshape(signs, N, 1)
+        psidY_mean .*= signs
 
-        G .+= C
-        psidY .+= 1
+        G = (g - g') / 2
 
-        h_off = ones(N)
+        diagonal = psidY_mean * ones(1, N)
+        off_diag  = diag(g) * ones(1, N)
+        h         = 0.5 .* (diagonal .+ diagonal' .- off_diag .- off_diag')
+        h = regularize_hessian(h, lambda_min)
 
-        # Hessian approximation diagonal (h)
-        
-        h = psidY' * Y_square ./ T
-        h = regularize_hessian(h, h_off, lambda_min)
-
-        G .-= Matrix{Float64}(I, N, N)
-
-        # Stopping criterion
         gradient_norm = maximum(abs.(G))
         if gradient_norm < tol
             requested_tolerance = true
             break
         end
 
-        # Update the L-BFGS memory
         if n > 1
             push!(s_list, direction)
             ΔG = G .- G_old
             push!(y_list, ΔG)
             push!(r_list, 1.0 / sum(direction .* ΔG))
-
             if length(s_list) > m
                 popfirst!(s_list)
                 popfirst!(y_list)
                 popfirst!(r_list)
             end
         end
-
         G_old = copy(G)
 
-        # Flush the memory if there is a sign change
         if sign_change
             current_loss = nothing
             empty!(s_list)
@@ -104,88 +75,77 @@ function ica_picard(dataset::sensorData)
             empty!(r_list)
         end
 
-        # Find the L-BFGS direction
-        direction = l_bfgs_direction(G, h, h_off, s_list, y_list, r_list, ortho=false)
+        direction = l_bfgs_direction(G, h, s_list, y_list, r_list, true)
 
-         # Do a line-search in that direction
-        converged, Y_new, W_new, new_loss, direction = line_search(Y, W, log_lik_tanh, direction, signs, current_loss; ls_tries=ls_tries, verbose=verbose, ortho=false, extended=true)
-
+        converged, Y_new, W_new, new_loss, direction =
+            line_search(Y, W, log_lik_tanh, direction, signs, current_loss;
+                        ls_tries=ls_tries, verbose=verbose, ortho=true, extended=false)
         if !converged
             direction .= -G
-
-            # flush the memory
             empty!(s_list)
             empty!(y_list)
             empty!(r_list)
-
-            _, Y_new, W_new, new_loss, direction = line_search( Y, W, log_lik_tanh, direction, signs, current_loss; ls_tries=10, verbose=false, ortho=false, extended=true)
+            _, Y_new, W_new, new_loss, direction =
+                line_search(Y, W, log_lik_tanh, direction, signs, current_loss;
+                            ls_tries=10, verbose=false, ortho=true, extended=false)
         end
 
         Y = Y_new
-        W = W_new
+        W = expm(direction) * W
         current_loss = new_loss
 
-        # Update covariance
-        C = W * covariance * W'
-
-        # Verbose logging
         if verbose
             println("iteration $(n), gradient norm = $(gradient_norm), loss = $(current_loss)")
         end
     end
 
-    infos = Dict(
-        :converged     => requested_tolerance,
-        :gradient_norm => gradient_norm,
-        :n_iterations  => n
-    )
-
-    infos[:signs] = signs
-
-    new_dataset = sensorData(dataset.time, Y)
-    return new_dataset
+    return sensorData(dataset.time, Y)
 end
 
-function compute_loss(Y::Matrix{Float64}, W::Matrix{Float64}, log_lik_tanh, signs::Vector{Float64}; ortho::Bool, extended::Bool)::Float64
-    T, N = size(Y)
-    loss = ortho ? 0.0 : -log(abs(det(W)))
-
-    for i in 1:N
-        y = Y[:, i]
-        s = signs[i]
-        loss += s * mean(log_lik_tanh.(y))
-        if extended && !ortho
-            loss += 0.5 * mean(y .^ 2)
-        end
-    end
-    return loss
-end
-
-function log_lik_tanh(x)
-    return -log.(cosh.(x))
-end
-
-function regularize_hessian(h::Matrix{Float64}, h_off::Vector{Float64}, lambda_min::Float64)::Matrix{Float64}
-    N = size(h, 1)
-    discr = sqrt.((h .- h').^2 .+ 4 .* (h_off * h_off'))
-
-    eigmat = 0.5 .* (h .+ h' .- discr)
-
-    mask = eigmat .< lambda_min
-    for i in 1:N
-        mask[i,i] = false
-    end
-
-    for (i,j) in CartesianIndices(mask)
-        if mask[i,j]
-            h[i,j] += (lambda_min - eigmat[i,j])
+function regularize_hessian(h::Matrix{Float64}, lambda_min::Float64)::Matrix{Float64}
+    for i in eachindex(h)
+        if h[i] < lambda_min
+            h[i] = lambda_min
         end
     end
     return h
 end
 
-function l_bfgs_direction(G::Matrix{Float64}, h::Matrix{Float64}, h_off::Vector{Float64}, s_list::Vector{Matrix{Float64}}, y_list::Vector{Matrix{Float64}}, r_list::Vector{Float64}, ortho::Bool)::Matrix{Float64}
+function score(Y::AbstractMatrix{Float64})
+    return tanh.(Y)
+end
 
+function score_der(psiY::AbstractMatrix{Float64})
+    return -mean(psiY .^ 2, dims=2) .+ 1.0
+end
+
+function gradient(Y::AbstractMatrix{Float64}, psiY::AbstractMatrix{Float64})
+    T = size(Y, 2)
+    return psiY * Y' ./ T
+end
+
+function proj_hessian_approx(Y::AbstractMatrix{Float64}, psidY_mean::AbstractVector{Float64}, G::AbstractMatrix{Float64})
+    N = size(Y, 2)
+    diagonal = psidY_mean * ones(1, N)
+    off_diag  = diag(G) * ones(1, N)
+    return 0.5 .* (diagonal .+ diagonal' .- off_diag .- off_diag')
+end
+
+function solve_hessian(G::AbstractMatrix{Float64}, h::AbstractMatrix{Float64})
+    return h \ G
+end
+
+function loss(Y::AbstractMatrix{Float64}, signs::AbstractVector{Float64})
+    N, T = size(Y)
+    total = 0.0
+    for i in 1:N
+        y = Y[i, :]
+        total += signs[i] * mean(abs.(y) .+ log1p.(exp.(-2.0 .* abs.(y))))
+    end
+    return total
+end
+
+function l_bfgs_direction(G::AbstractMatrix{Float64}, h::AbstractMatrix{Float64}, s_list::Vector{Matrix{Float64}}, y_list::Vector{Matrix{Float64}}, r_list::Vector{Float64}, ortho::Bool)
     q = copy(G)
     a_list = Float64[]
     for (s, y, r) in Iterators.reverse(zip(s_list, y_list, r_list))
@@ -193,58 +153,34 @@ function l_bfgs_direction(G::Matrix{Float64}, h::Matrix{Float64}, h_off::Vector{
         push!(a_list, α)
         q .-= α .* y
     end
-
-    if ortho
-        z = q ./ h
-        z .= (z .- z') ./ 2
+    z = if ortho
+        q ./ h
     else
-        z = h \ q
+        h \ q
     end
-
-    for ((s, y, r), α) in zip(zip(s_list, y_list, r_list), Iterators.reverse(a_list))
+    for (s, y, r, α) in zip(s_list, y_list, r_list, Iterators.reverse(a_list))
         β = r * sum(y .* z)
         z .+= (α - β) .* s
     end
-
     return -z
 end
 
-function line_search(Y::AbstractMatrix{Float64}, W::AbstractMatrix{Float64}, log_lik::Function, direction::AbstractMatrix{Float64}, signs::AbstractVector{Float64}, current_loss::Union{Nothing,Float64}, ls_tries::Int, verbose::Bool, ortho::Bool, extended::Bool)
-    
-    N = size(W, 1)
+function line_search(Y::AbstractMatrix{Float64}, W::AbstractMatrix{Float64}, log_lik::Function, direction::AbstractMatrix{Float64}, signs::AbstractVector{Float64}, current_loss::Union{Nothing, Float64}; ls_tries::Int, verbose::Bool, ortho::Bool, extended::Bool)
     α = 1.0
-
     if current_loss === nothing
-        current_loss = compute_loss(Y, W, log_lik, signs; ortho=ortho, extended=extended)
+        current_loss = loss(Y, signs)
     end
-
     Y_new = similar(Y)
     W_new = similar(W)
-    new_loss = current_loss
-
     for _ in 1:ls_tries
-        if ortho
-            transform = exp(α * direction)
-        else
-            transform = I + α * direction
-        end
-
-        # пробен ъпдейт
+        transform = ortho ? expm(α .* direction) : I + α .* direction
         Y_new .= transform * Y
         W_new .= transform * W
-
-        new_loss = compute_loss(Y_new, W_new, log_lik, signs; ortho=ortho, extended=extended)
-
+        new_loss = loss(Y_new, signs)
         if new_loss < current_loss
             return true, Y_new, W_new, new_loss, α .* direction
         end
-
         α /= 2
     end
-
-    if verbose
-        @warn "line search failed, falling back to gradient"
-    end
-
-    return false, Y_new, W_new, new_loss, α .* direction
+    return false, Y_new, W_new, current_loss, α .* direction
 end
